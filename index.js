cbc55b45c19a38c93512a93a6a8e19e6713022c2
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-10-01 19:48:59 -0400

    Code works now. Was able to have index.js activate the user_input file, take that information then insert it into the shapes file and then write that code to the logo.svg with the required criteria. Cleaned up the code a bit and added some missing comments

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
+// calling the needed files
 const UserInput = require('./lib/user_input.js')
 const Shapes = require('./lib/shape.js')
+const fs = require('fs')
 
 // creating an async function to wait for the logo to be called so that I can run the user input file
 async function logo() {
+
+    // calling a new instance of user input
     const userInput = new UserInput();
+
+    // taking the inputed userdata putting it under this constant to sort through
     const userData = await userInput.run();
     
-    // logging the data from user input. 
-    // console.log('User Input Data:', userData);
-    // console.log('text:', userData.text);
-    // console.log('text color:', userData.textColor.toLowerCase());
-    // console.log('shape:', userData.shape.toLowerCase());
-    // console.log('shape color:', userData.shapeColor.toLowerCase());
-    
+    // assigning the information to const
     const text = userData.text;
     const textColor = userData.textColor;
     const shapeColor = userData.shapeColor
     var shape = userData.shape.toLowerCase()
 
+    // creating a blank var for the shape text literal to go into
+    var logoContent
+
     const shapes = new Shapes()
-    if (shape == 'circle')
-        console.log('shape.circle')
-        console.log(shapes.circle(text, textColor, shapeColor))
-  }
+
+    // if loop to select the appropriate shape template
+    if (shape == 'circle') {
+        logoContent = shapes.circle(text, textColor, shapeColor)
+
+    } else if  ( shape == 'rectangle') {
+        logoContent = shapes.rectangle(text, textColor, shapeColor)
+
+    } else {
+        logoContent = shapes.triangle(text, textColor, shapeColor)
+    }
+
+    // writing logoContent to the logo.svg file
+    fs.writeFile('logo.svg',logoContent, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Generated logo.svg')
+        }
+    });
+  };
   
   logo();
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,14 +1,18 @@
 const Shape = require('../lib/shape.js')
 
+// calling the Shape.prototype.circle
 describe( 'Shape.prototype.circle', () => {
     it('should generate SVG code for a circle with text', () => {
+        // adding in const to test the insertion is correct
         const shape = new Shape();
         const text = 'Text';
         const textColor = 'red';
         const shapeColor = 'blue';
 
+        //saving the shape function to const to check it that the text is correct. 
         const svgCode = shape.circle(text, textColor, shapeColor);
 
+        // using .toContain to check if the svg has the line of entry with the updated input
         expect(svgCode).toContain(`<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`);
         expect(svgCode).toContain(`<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`);
     })
